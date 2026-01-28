08c55bcab3eed22f15d6d8670289ffb922be1c21
Debby-arch <opiyorosedebrah@gmail.com>
2026-01-28 04:04:15 +0300

    Home and Not found pages

---
+++ src/pages/Home.jsx
@@ -0,0 +1,15 @@
+
+export const Home = () => {
+  return  
+    <div className="min-h-screen ">
+        {/* Theme toggle(dark/light) */}
+
+        {/*Background Effects*/}
+
+        {/*Navbar */}
+
+        {/*Main Content */}
+
+        {/*Footer */}
+    </div>;
+};
