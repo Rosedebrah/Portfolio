cc3582ec5fda43c533deca2c81949eb969936963
Debby-arch <opiyorosedebrah@gmail.com>
2026-02-01 02:29:51 +0300

    initial commit

---
+++ app/layout.tsx
@@ -0,0 +1,34 @@
+import { Outfit, Ovo } from "next/font/google";
+import "./globals.css";
+import { Metadata } from "next";
+
+const outfitFont = Outfit({
+  subsets: ["latin"], 
+  weight: ["400", "500", "600", "700"]
+});
+
+const ovoFont = Ovo({
+  subsets: ["latin"], 
+  weight: ["400"]
+});
+
+export const metadata: Metadata = {
+  title: "Portfolio - Rosedebrah",
+  description: "",
+};
+
+export default function RootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+    <html lang="en">
+      <body
+        className={`${outfitFont.className} ${ovoFont.className} antialiased`}
+      >
+        {children}
+      </body>
+    </html>
+  );
+}
