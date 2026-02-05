c3350d2dcb6b87ba017d74ba559d55d4069a3c50
Debby-arch <opiyorosedebrah@gmail.com>
2026-02-05 05:41:39 +0300

    footer

---
+++ app/components/Footer.jsx
@@ -0,0 +1,28 @@
+import React from 'react'
+import Image from 'next/image'
+import {assets} from '@/assets/assets'
+
+const Footer = () => {
+  return (
+    <div className='mt-20'>
+      <div className='text-center'>
+        <Image src={assets.logo} alt='' className='w-36 mx-auto mb-2'/>
+
+        <div className='w-max flex items-center gap-2 mx-auto'>
+          <Image src={assets.mail_icon} alt='' className='w-6'/> 
+          ojuokrosedebrah@gmail.com
+        </div>
+      </div>
+
+      <div className='text-center sm:flex items-center justify-between border-t border-gray-400 mx-[10%] mt-12 py-6'>
+        <p>© 2026 Rosedebrah Ojuok. All rights reserved. </p>
+        <ul className='flex items-center gap-10 justify-center mt-4 sm:mt-0'>
+            <li><a target='_blank' href="https://github.com/Rosedebrah">Github</a></li>
+            <li><a target='_blank' href="https://www.linkedin.com/in/rosedebrahojuok/">LinkedIn</a></li>
+        </ul>
+      </div>
+    </div>
+  )
+}
+
+export default Footer
--- app/page.tsx
+++ app/page.tsx
@@ -5,6 +5,8 @@ import About from "./components/About";
 import Services from "./components/Services";
 import Work from "./components/Work";
 import Contact from "./components/Contact";
+import Footer from "./components/Footer";
+
 
 export default function Home() {
   return (
@@ -15,6 +17,7 @@ export default function Home() {
     <Services />
     <Work />
     <Contact />
+    <Footer />
 
     </>
   );
