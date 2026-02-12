cd2f48214a4496e867d8e50757fbb753c05d937a
Debby-arch <opiyorosedebrah@gmail.com>
2026-02-12 14:22:42 +0300

    add service page component

---
+++ app/services/[id]/page.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+
+
+
+export default async function page({ params }){
+
+const { id } = await params;
+
+  return (
+    <div>
+      <h2>Service{id}</h2>
+    </div>
+  )
+}
+
+
